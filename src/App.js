6dbbd20f072b36708ef773990899da69fe0d1916
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-09 09:02:45 +0900

    added header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-
+    <Header />
     </div>
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
