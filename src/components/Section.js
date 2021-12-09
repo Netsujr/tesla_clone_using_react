361d49852176d8e78ba57793ba4700c2bacc932b
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-09 09:28:12 +0900

    added images

--- src/components/Section.js
+++ src/components/Section.js
@@ -12,5 +12,10 @@ const Section = () => {
 export default Section;
 
 const Wrap = styled.div`
-
+width: 100vw;
+height: 100vh;
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+background-image: url('/images/model-s.jpg')
 `
