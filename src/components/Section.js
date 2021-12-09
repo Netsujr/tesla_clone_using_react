bea8b93d6b90a86a8c53764c730f7d38158ad427
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-09 09:47:09 +0900

    styled buttons at bottom of page

--- src/components/Section.js
+++ src/components/Section.js
@@ -28,20 +28,37 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url('/images/model-s.jpg')
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
   `
 
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
-
   `
 
 const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
 
   `
 const LeftButton = styled.div`
-
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
   `
 const RightButton = styled(LeftButton)`
 
